d3a18000a5d7ad3f730cb6a79a72a7dc20aa7518
David Snopek <dsnopek@gmail.com>
2010-03-26 10:40:27 +0100

    Got the annotation reader bubble showing in the sidebar.

--- lingwo_korpus/annotation-reader.js
+++ lingwo_korpus/annotation-reader.js
@@ -1,14 +1,74 @@
 
 (function () {
-    var bubble;
+    var bubble, contentArea, dock, footer, popup = false, setGlobalEvents = false,
+        footerId, footerMargin;
+
+    function clobber(obj, old_fn_name, new_fn) {
+        old_fn = obj[old_fn_name];
+        obj[old_fn_name] = function () {
+            old_fn.apply(this, arguments);
+            new_fn();
+        };
+    }
+
     Drupal.behaviors.lingwo_korpus = function (context) {
-        
         if (!bubble) {
             bubble = $('<div></div>')
+                .attr('id', 'lingwo-korpus-entry-view-wrapper')
+                .addClass(popup ? 'popup' : 'docked')
+                .appendTo($('body'));
+        }
+        if (!contentArea) {
+            contentArea = $('<div></div>')
                 .attr('id', 'lingwo-korpus-entry-view')
-                .appendTo($('body'))
-                .hide();
-        };
+                .appendTo(bubble);
+        }
+        if (!dock) {
+            dock = $('#lingwo-korpus-dock', context);
+            footerId = dock.attr('data-footer-id');
+            footerMargin = dock.attr('data-footer-margin');
+        }
+        if (!footer) {
+            footer = $('#'+footerId, context);
+        }
+
+        function positionBubble() {
+            if (popup) return;
+
+            var offset = dock.offset(),
+                footerTop = footer.offset().top,
+                paddingX = bubble.outerWidth() - bubble.innerWidth(),
+                paddingY = bubble.outerHeight() - bubble.innerHeight(),
+                top = Math.max(offset.top - $(window).scrollTop() + 1, 0),
+                height = Math.min(
+                    footerTop - $(window).scrollTop() - top - footerMargin + 1, 
+                    $(window).height() - top) - paddingY;
+
+            bubble.css({
+                'left': offset.left - $(window).scrollLeft(),
+                'top': top,
+                'height': height
+            });
+        }
+
+        if (!popup) {
+            // TODO: this should use the theme system somehow!!
+            contentArea.html('<em>Click a word in the text to look it up in the dictionary.</em>');
+            setTimeout(positionBubble, 0);
+        }
+
+        // these should be set once and only once
+        if (!setGlobalEvents) {
+            $(window).scroll(positionBubble);
+            $(window).resize(positionBubble);
+            // Special support for the 'admin' module.
+            if (Drupal.adminToolbar) {
+                clobber(Drupal.adminToolbar, 'setState', function () {
+                    setTimeout(positionBubble, 500);
+                });
+            }
+            setGlobalEvents = true;
+        }
 
         // handle clicks on the annotations
         var selected = null;
@@ -22,16 +82,18 @@
 
             if (target.hasClass('anno')) {
                 offset = target.offset();
-                bubble.show()
-                    // X-centered and 40px down
-                    // TODO: we really should limit it to existing inside the viewable
-                    // area.  We don't want it going outside and forcing scrollbars to appear.
-                    .css({
-                        left: (offset.left + (target.width() / 2) - 150) + 'px',
-                        top: (offset.top + target.height() + 40) + 'px' });
+                if (popup) {
+                    bubble.show()
+                        // X-centered and 40px down
+                        // TODO: we really should limit it to existing inside the viewable
+                        // area.  We don't want it going outside and forcing scrollbars to appear.
+                        .css({
+                            left: (offset.left + (target.width() / 2) - 150) + 'px',
+                            top: (offset.top + target.height() + 40) + 'px' });
+                }
 
                 pos = target.attr('data-pos');
-                bubble.html('Loading ...');
+                contentArea.html('Loading ...');
 
                 // mark as selected
                 selected = target;
@@ -40,12 +102,12 @@
                 // lookup the entry on the server
                 $.getJSON('/lingwo_korpus/lookup_entry', {'url': target.attr('href')},
                     function (res) {
-                        bubble.html(res.content);
-                        $('div.node', bubble).removeClass('clear-block');
+                        contentArea.html(res.content);
+                        $('div.node', contentArea).removeClass('clear-block');
                     }
                 );
             }
-            else {
+            else if (popup) {
                 bubble.hide();
             }
 
