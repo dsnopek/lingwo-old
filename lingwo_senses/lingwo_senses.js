5c979ceb83371f65ab5a088e939668da1d61493f
David Snopek <dsnopek@gmail.com>
2010-04-10 17:21:24 +0200

    * Senses now have a "No equivalent" option.  * Fixed HTML in our sample-annotated-text.xml

--- lingwo_senses/lingwo_senses.js
+++ lingwo_senses/lingwo_senses.js
@@ -1,6 +1,8 @@
 
 Drupal.behaviors.lingwo_senses = function (context) {
-    function toggle(node, anim) {
+    // TODO: do some slick functional programming and combine this clearly copy-paste coding
+
+    function toggle_sameas(node, anim) {
         var id = (node.id+'').replace(/same-as/, 'translation'),
             show = ($(':selected', node).val() == '');
 
@@ -8,8 +10,20 @@ Drupal.behaviors.lingwo_senses = function (context) {
     }
 
     $('.same-as-select', context).change(function (evt) {
-        toggle(evt.target, true);
+        toggle_sameas(evt.target, true);
     });
-    $('.same-as-select', context).each(function (i, node) { toggle(node); });
+    $('.same-as-select', context).each(function (i, node) { toggle_sameas(node); });
+
+    function toggle_noequiv(node, anim) {
+        var id = (node.id+'').replace(/no-equivalent/, 'translation-trans'),
+            show = !$(node).is(':checked');
+
+        $('#'+id)[show ? 'show' : 'hide'](anim ? 'fast' : null);
+    }
+
+    $('.no-equivalent-checkbox', context).change(function (evt) {
+        toggle_noequiv(evt.target, true);
+    });
+    $('.no-equivalent-checkbox', context).each(function (i, node) { toggle_noequiv(node); });
 };
 
