63744211e771ab076cb2f4acafbbc8b36fade0e5
David Snopek <dsnopek@gmail.com>
2010-05-03 13:04:06 +0200

    Started implementing mode switching in the annotator.

--- lingwo_korpus/annotator.js
+++ lingwo_korpus/annotator.js
@@ -2,26 +2,48 @@
 // provides our annotator
 
 (function () {
-    var selector;
+    var selector, reader, mode = 'edit';
 
     // IE hack for un-recognized elements
     document.createElement('word');
     document.createElement('grammar');
 
-    function showDialog(node, contentArea, setContentFunc) {
+    function setEditMode(_mode) {
+        // change the display
+        $('#edit-korpus-text').removeClass('mode-edit').removeClass('mode-add').addClass('mode-'+_mode);
+
+        mode = _mode;
+        switch (mode) {
+            case 'edit':
+                break;
+            case 'add':
+                break;
+        };
+    };
+
+    function showAnnotation(node, contentArea, setContentFunc) {
         alert(node);
     }
 
     Drupal.behaviors.lingwo_korpus = function (context) {
-        $('word', context).addClass('anno');
         require([
             'lingwo_dictionary/annotation/Reader',
             'lingwo_dictionary/annotation/TextSelector'],
             function (Reader, TextSelector) {
-                Reader.setup(context, showDialog);
-                if (typeof selector === 'undefined') {
-                    //selector = new TextSelector(document.getElementById('edit-korpus-text'));
+                // setup the reader
+                if (typeof reader === 'undefined') {
+                    Reader.setup(context, showAnnotation);
+                    reader = Reader;
                 }
+                // setup the text selector
+                if (typeof selector === 'undefined') {
+                    selector = new TextSelector(document.getElementById('edit-korpus-text'));
+                }
+
+                // setup the mode controls
+                function handleMode() { setEditMode($('input[name=edit-mode]:checked').val()); };
+                $('input[name=edit-mode]:radio', context).click(handleMode);
+                handleMode();
             }
         );
     };
