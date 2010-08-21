dcb65b181edf0e5a470d56f31ef0ddf208857d9e
David Snopek <dsnopek@gmail.com>
2010-08-21 21:54:44 +0200

    * Got the Python NLP code connecting to our lingwo_dictionary.search_entries() RPC.  * Added some data to the RPC's return (pos, headword, language)

--- nlp/autolookup.py
+++ nlp/autolookup.py
@@ -1,13 +1,18 @@
 
 from LingwoNLP.document import parse
+from LingwoNLP.remote import ServerInterface
 
 def main():
-    doc = parse(open('text1-clean.txt', 'rt'))
-    doc.segmentize()
-
-    for stream in doc.sents:
-        for token in stream.tokens:
-            print token
+#    doc = parse(open('text1-clean.txt', 'rt'))
+#    doc.segmentize()
+#
+#    for stream in doc.sents:
+#        for token in stream.tokens:
+#            print token
+    
+    remote = ServerInterface()
+    remote.login()
+    print remote.call('lingwo_dictionary.search_entries', 'dogs')
 
 if __name__ == '__main__': main()
 
