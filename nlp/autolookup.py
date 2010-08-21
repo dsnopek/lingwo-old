99bcf1d23ea955331660fdf2fccaa64088d26076
David Snopek <dsnopek@gmail.com>
2010-08-21 22:06:55 +0200

    Got our autolookup.py script actually lookup words and setting them as advertised.

--- nlp/autolookup.py
+++ nlp/autolookup.py
@@ -3,16 +3,36 @@ from LingwoNLP.document import parse
 from LingwoNLP.remote import ServerInterface
 
 def main():
-#    doc = parse(open('text1-clean.txt', 'rt'))
-#    doc.segmentize()
-#
-#    for stream in doc.sents:
-#        for token in stream.tokens:
-#            print token
-    
     remote = ServerInterface()
     remote.login()
-    print remote.call('lingwo_dictionary.search_entries', 'dogs')
+
+    # TODO: this isn't what we want to do in the end, but it will work now for testing
+    def qlookup(word):
+        res = remote.call('lingwo_dictionary.search_entries', word, {'language':'en'})
+        if len(res) > 0:
+            return res[0]
+        return None
+
+    doc = parse(open('text1-clean.txt', 'rt'))
+    doc.segmentize()
+
+    for stream in doc.sents:
+        for token in stream.tokens:
+            word = str(token)
+
+            # try first the word, then a lower case version of the word
+            res = qlookup(word)
+            if res is None:
+                res = qlookup(word.lower())
+
+            if res is not None:
+                if word != res['headword']:
+                    token.dom.setAttribute('headword', res['headword'])
+                token.dom.setAttribute('pos', res['pos'])
+                if res.has_key('form_name'):
+                    token.dom.setAttribute('form', res['form_name'])
+
+    print doc.dom.toxml()
 
 if __name__ == '__main__': main()
 
