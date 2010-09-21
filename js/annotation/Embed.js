8f123d0d650cc7930e9c964c7fc28bae5852fd5b
David Snopek <dsnopek@gmail.com>
2010-09-21 22:21:37 +0200

    Began actually implementing the code to embed Bibliobird in another page.

--- js/annotation/Embed.js
+++ js/annotation/Embed.js
@@ -1,6 +1,25 @@
 
-require.def('lingwo_dictionary/annotation/Embed', ['jquery'],
-    function ($) {
+require.def('lingwo_dictionary/annotation/Embed',
+    ['jquery','lingwo_dictionary/annotation/Reader'],
+    function ($, Reader) {
+        function loadEntry(target, contentArea, setContentFunc) {
+            alert('loadEntry');
+        }
+
+        $('.bibliobird-content').each(function (i, x) {
+            var from_lang = $(x).attr('data-from-lang'),
+                to_lang   = $(x).attr('data-to-lang'),
+                url       = $(x).attr('data-url') || window.location.href,
+                teaser    = $(x).attr('data-teaser') == 'true';
+            
+            // TODO: we need to JSONP the annotated text of the node over
+
+            // TODO: we need to create the reader after this has loaded
+            Reader.setup(x, loadEntry, true);
+            $('body', x).click(function (evt) {
+                return Reader.onClick(evt);
+            });
+        });
     }
 );
 
