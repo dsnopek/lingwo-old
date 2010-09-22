4073aafa6cc7d4a3045592f3640aab8d91bcb71a
David Snopek <dsnopek@gmail.com>
2010-09-22 13:03:14 +0200

    Fair attempt at doing remote login.

--- js/annotation/Embed.js
+++ js/annotation/Embed.js
@@ -3,10 +3,35 @@ require.def('lingwo_dictionary/annotation/Embed',
     ['jquery','lingwo_dictionary/annotation/Reader'],
     function ($, Reader) {
         //var bibliobird_url = 'http://www.bibliobird.com';
-        var bibliobird_url = 'http://localhost:35637';
+        var bibliobird_url = 'http://localhost:35637',
+            embedWindow = $('<div></div>'),
+            embedIframe = $('<iframe></iframe>');
 
+        // make the embedWindow / embedIframe
+        embedIframe
+            .css({
+                width: 600,
+                height: 400
+            })
+            .appendTo(embedWindow);
+        embedWindow
+            .css({
+                width: 600,
+                height: 400,
+                position: 'fixed',
+            })
+            .hide()
+            .appendTo($('body'));
+        function positionEmbedWindow() {
+            var left = ($(window).width() - 600) / 2,
+                top  = ($(window).height() - 400) / 2;
+            embedWindow.css({ top: top, left: left });
+        }
+        $(window).resize(positionEmbedWindow);
+        setTimeout(positionEmbedWindow, 0);
 
         // Debuging!  Cache busting!
+        /*
         var oldAttach = require.attach;
         require.attach = function (url, contextName, moduleName, callback, type) {
            url += (url.indexOf('?') === -1 ? '?' : '&') + 'bust=' + (new
@@ -14,6 +39,7 @@ require.def('lingwo_dictionary/annotation/Embed',
            return oldAttach.call(require, url, contextName, moduleName,
         callback, type);
         }
+        */
 
         function loadEntry(target, contentArea, setContentFunc) {
             var sense_id = target.attr('data-sense');
@@ -49,6 +75,11 @@ require.def('lingwo_dictionary/annotation/Embed',
             });
         }
 
+        function open_embedded(url) {
+            embedIframe.attr('src', url+'?embed=1');
+            embedWindow.show();
+        }
+
         $('.bibliobird-content').each(function (i, x) {
             var from_lang = $(x).attr('data-from-lang'),
                 to_lang   = $(x).attr('data-to-lang'),
@@ -56,16 +87,48 @@ require.def('lingwo_dictionary/annotation/Embed',
                 teaser    = $(x).attr('data-teaser') == 'true',
                 links     = $('<div class="bibliobird-links"></div>').insertBefore(x);
 
-            function onerror() {
-                links.append($('<a></a>')
-                    .html('Add to Bibliobird')
-                    // TODO: we need some configuration, so we can point the user to the
-                    // correct language site!
-                    .attr({
-                        href: bibliobird_url+'/node/add/content?remote_url='+escape(url),
-                        target: '_blank'
-                    })
-                );
+            function build_links(res) {
+                links.html('');
+
+                if (res.username) {
+                    links.append('Logged into BiblioBird as '+res.username+' ');
+                    /*
+                    links.append($('<a></a>')
+                        .html('Logout')
+                        .attr('href', bibliobird_url+'/logout')
+                        // TODO: I think we need a special JSONP logout function, because we need to know when
+                        // its finished
+                        .click(function () { return false })
+                    );
+                    */
+                }
+                else {
+                    links.append('Not logged into BiblioBird ');
+                    links.append($('<a></a>')
+                        .html('Login')
+                        .attr('href', bibliobird_url+'/user/login')
+                        .click(function (evt) { open_embedded(evt.target.href); return false; })
+                    );
+                    links.append(' ');
+                    links.append($('<a></a>')
+                        .html('Join BiblioBird')
+                        .attr('href', bibliobird_url+'/user/register')
+                        .click(function (evt) { open_embedded(evt.target.href); return false; })
+                    );
+                }
+                links.append(' ');
+
+                if (res.not_found) {
+                    links.append($('<a></a>')
+                        .html('Add to Bibliobird')
+                        // TODO: we need some configuration, so we can point the user to the
+                        // correct language site!
+                        .attr({
+                            href: bibliobird_url+'/node/add/content?remote_url='+escape(url),
+                            target: '_blank'
+                        })
+                    );
+                }
             }
 
             // we need to JSONP get the annotated text of the node
@@ -74,20 +137,18 @@ require.def('lingwo_dictionary/annotation/Embed',
                 dataType: 'jsonp',
                 data: { url: url, to_lang: to_lang },
                 success: function (res) {
-                    if (res.error) {
-                        onerror();
-                        return;
-                    }
+                    build_links(res);
 
-                    // assign the content
-                    x.innerHTML = res.content;
-                    
-                    Reader.setup(x, loadEntry, true);
-                    $('body').click(function (evt) {
-                        return Reader.onClick(evt);
-                    });
-                },
-                error: onerror
+                    if (res.content) {
+                        // assign the content
+                        x.innerHTML = res.content;
+                        
+                        Reader.setup(x, loadEntry, true);
+                        $('body').click(function (evt) {
+                            return Reader.onClick(evt);
+                        });
+                    }
+                }
             });
         });
     }
